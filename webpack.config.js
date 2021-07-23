66216ec0c352d4a8aa5cdeb6695ce9a501d3dec7
woow_wu7 <wuxia@shouqianba.com>
2021-07-23 18:20:40 +0800

    feat: hash type

--- webpack.config.js
+++ webpack.config.js
@@ -22,22 +22,64 @@ module.exports = {
   //      - 多页应用mpa: 多个入口起点
   //  3. 如果entry后面跟一个 ( 字符串 )，或者 ( 字符串数组 )，chunk会被命名为 ( main ) ---- main
   //  4. 如果entry后面跟一个 ( 对象 )，则 ( key ) 就是 thunk名 -------------------------- key
+
+  //  5. 如何打包一个多页应用
+  //      - 1. entry设置为 ( 对象模式 )，则可以指定 ( 多个入口 )
+  //      - 2. output的 ( filename ) 设置为 ('[name].[hash:8].js' ) 的形式，使用 ( 占位符 ) 则可以分别打包为不同的 ( 出口文件 )
+  //      - 3. plugins 数组中需要多次 ( new HtmlWebpackPlugin() )，具体如下
+            // plugins: [
+            //   new HtmlWebpackPlugin({ // ---------------------------- html-webpack-plugin可以new多个
+            //     template: './src/index.html', // 模版html
+            //     filename: 'home.html', // 打包后的html文件名
+            //     chunks: ['home'] // --------------------------------- 每个chunk对应加载哪些打包后的 js 文件，即 output指定的输出js文件
+            //   }),
+            //   new HtmlWebpackPlugin({
+            //     template: './src/index.html',
+            //     filename: 'other.html',
+            //     chunks: ['other']
+            //   }),
+            // ]
+
   entry: {
     main: './src/index.js', // 这里entry是一个对象，main 就是打包后的 thunk 名称
+    other: './src/other.js'
   },
 
 
   // output
-  //  1. filename 
+  //  1. filename
   //      - filename: 表示打包后的 thunk 的名字
   //      - '[name].[hash:8].js'
-  //        - []: 表示占位符号
+  //        - []: 表示占位符
   //        - [name]: 表示使用 entry 属性对象中的 key 作为thunk名
   //        - [hash:8]: 表示加上hash串，长度为 8
   //  2. path
   //      - path: 表示打包生成的文件夹的路径
+  //  3. hash chunkhash contenthash 之间的区别？
+  //      - hash
+  //          - 作用：只要项目中有文件修改，整个项目构建的hash都会改变，并且全部文件都共用相同的hash
+  //          - 弊端：如果只修改了一个文件，整个文件的缓存都将失效，因为真个文件的hash都改变了
+  //      - chunkhash
+  //          - 相对于hash，chunkhash的影响范围较小
+  //          - 原理：
+  //            - 根据不同的入口文件(Entry)进行依赖文件解析、构建对应的chunk，生成对应的哈希值
+  //            - 不同入口打包生成的chunk的hash不一样
+  //          - 测试
+  //            - 请使用 cnpm run build 进行 chunkhash 的测试，main和other的js文件的hash值就不一样
+  //          - 例子：
+  //            - 策略：比如一个项目有6个组件，123打包为一个thunk1输出一组js/css，456打包为另一个thunk2输出另一组js/css
+  //            - 结果： 如果使用chunkhash，打包完成后chunk1的hash和chunk2的hash就不一样，改动了123，456的chunk2的hash就不会变，缓存仍然有效
+  //      - contenthash
+  //          - 1. 影响范围最小，在hash，chunkhash，contenthash三者中
+  //          - 2. 遇到问题
+  //            - 使用chunkhash，如果index.css被index.js引用了，那么 ( css文件和js文件 ) 就会 ( 共用相同的chunkhash值 )
+  //            - 如果index.js更改了代码，css文件就算内容没有任何改变，由于是该模块发生了改变，导致css文件会重复构建
+  //          - 3. 解决方法
+  //            - 使用extra-text-webpack-plugin里的contenthash值，保证即使css文件所处的模块里就算其他文件内容改变，只要css文件内容不变，那么不会重复构建。
   output: {
-    filename: '[name].[hash:8].js',
+    // filename: '[name].[hash:8].js',
+    filename: '[name].[chunkhash:8].js',
+    // filename: '[name].[content:8].js',
     path: path.resolve(__dirname, 'build')
   },
 
@@ -51,7 +93,7 @@ module.exports = {
   //      - contentBase需要和output.path保持一致
   //  3. host主机 port端口 compress开启gzip压缩 hot开启热更新
   devServer: { // 开发服务器配置项
-    contentBase: path.join(__dirname, 'build'), 
+    contentBase: path.join(__dirname, 'build'),
     host: 'localhost', // 主机，可以从外部访问
     port: 5555,
     compress: true, // 开启 gzip 压缩
@@ -65,7 +107,7 @@ module.exports = {
     //     },
     //     // https相关
     //     // 可以发送 https，默认使用自签名证书，也可以提供自己的证书，如下三个属性 key cert ca
-    //     https: true, 
+    //     https: true,
     //     key: fs.readFileSync('/path/to/server.key'),
     //     cert: fs.readFileSync('/path/to/server.crt'), // certificate 证书的缩写
     //     ca: fs.readFileSync('/path/to/ca.pem'),
@@ -78,17 +120,17 @@ module.exports = {
   // module
   //  1. loader --------------------------------------------------------------------- loader
   //      - loader顺序: 从右往左，从下往上
-  //      - 比如css相关的loader顺序: sass-loadder => css-loader => style-loader
+  //      - 比如css相关的loader顺序: sass-loader => css-loader => style-loader
   //  2. css相关的loader
   //      - style-loader 主要用来解析 ( @import ) 语法
   //      - css-loader 将css插入到html的 ( head ) 部分
   //      - sass-loader node-sass less
-  //      - 问题: 
+  //      - 问题:
   //        - 1. 如果只用 ( style-loader和css-loader ) 会直接插入到HTML中
   //          - 可以用webpack-dev--server启动服务在html中f12查看elements，css插入到了head部分
   //          - 注意，并不是插入到打包后的html中，而是要启动服务才会看到到
   //        - 2. 如何使用sass？？？？
-  //          - sass-loader sass node-sass 
+  //          - sass-loader sass node-sass
   //          - 先用 sass-loader => css => css-loader => style-loader => 插入html的head
   //        - 3. 那么要如何把 css 抽离成单独的文件来引入呢？？？？
   //          - mini-css-extract-plugin 单独抽离css文件
@@ -102,7 +144,7 @@ module.exports = {
   //            - autoprefixer需要给出浏览器的一些信息，需要在package.json中配置 browsersList 属性
   //  3. 图片相关loader
   //      - file-loader
-  //      - url-loader // 当图片小于阈值时转成base64，大于于阈值时使用file-laoder
+  //      - url-loader // 当图片小于阈值时转成base64，大于于阈值时使用file-loader
   module: {
     rules: [
       {
@@ -128,7 +170,7 @@ module.exports = {
           {loader: 'postcss-loader'}, // 先处理sass => 处理前缀 => 处理@import => 抽离css
           {loader: 'sass-loader'},
         ]
-      }, 
+      },
       {
         test: /\.js$/,
         use: [
@@ -146,7 +188,7 @@ module.exports = {
             //   ]
             // }
           }
-        ] 
+        ]
       },
       {
         test: /\.(png|jpg|jpeg|git)$/,
@@ -192,6 +234,17 @@ module.exports = {
       //   removeAttributeQuotes: true, // 删除html属性的双引号
       //   collapseWhitespace: true, // 将html折叠成一行
       // }
+      // chunks: ['home']
+    }),
+    new HtmlWebpackPlugin({ // 打包多页应用时，可以指定 chunks
+      template: './src/index.html',
+      filename: 'other.html', // 打包过后的html的文件名
+      hash: true, // 在打包后的build文件夹中的html文件引入资源时，是否加hash串
+      // minify: {
+      //   removeAttributeQuotes: true, // 删除html属性的双引号
+      //   collapseWhitespace: true, // 将html折叠成一行
+      // }
+      // chunks: ['other']
     }),
     new MiniCssExtractPlugin({
       filename: 'css/main.css', // 指定被打包后的文件夹，和文件名
@@ -216,3 +269,7 @@ module.exports = {
     ]
   }
 }
+
+
+// bundle
+// 1. bundle由chunk组成
